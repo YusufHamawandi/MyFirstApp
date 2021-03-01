0a99c1f2d2a2edcf905b26b5fad54262c9f22f5c
Yusuf Hamawandi <yusuf.hamawandi@outlook.com>
2021-03-01 18:51:46 +0100

    Update on my project

--- MyFirstApp/ConsoleApp1/ConsoleApp1/ConsoleApp1/Program.cs
+++ MyFirstApp/ConsoleApp1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,32 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Title = "Calculator";
+            Console.ForegroundColor = ConsoleColor.Green;
+            
+            int value1;
+            int value2;
+
+            value1 = GetValue("What Is Your First Value?");
+
+            value2 = GetValue("What Is Your Secound Value?");
+            
+            WriteResult(value1, value2);
+
+        }
+        static int GetValue(string message)
+        {
+            int value;
+            Console.WriteLine(message);
+            value = Convert.ToInt32(Console.ReadLine());
+            return value;
+        }
+        static void WriteResult(int v1, int v2)
+        {
+            int result = v1 + v2;
+            Console.WriteLine("Your Result is:");
+            Console.WriteLine(result.ToString());
+
         }
     }
 }
